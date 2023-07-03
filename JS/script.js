cb3ab916d76a0727432511a5f7dc2fce0dcc4315
ავთანდილ საგინაშვილი <120257059+AvtandilSaginashvili@users.noreply.github.com>
2023-07-03 22:00:37 +0400

    update

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,5 @@
 function create() {
     let name = document.getElementById("nameinput");
-    let email = document. getElementById("emailinput");
     let password = document.getElementById("passwordinput");
     let alert = document.getElementById("woop");
 
@@ -13,15 +12,6 @@ function create() {
         name.style.border = "1px solid transparent";
     }
 
-    if ( email.value == "") {
-        email.style.border = "1px solid red";
-        document.getElementById("woop2").innerHTML = "Input is empty or insert '@'";
-        document.getElementById("woop2").style.color = "red";
-    }else {
-        document.getElementById("woop2").innerHTML = "";
-        email.style.border = "1px solid transparent";
-    }
-
     if (password.value == "") {
         password.style.border = "1px solid red"
         document.getElementById("woop3").innerHTML = "input is empty";
@@ -32,3 +22,21 @@ function create() {
     }
     
 }
+
+const validation = document.getElementById("forma");
+validation.addEventListener("submit", e =>{
+    e.preventDefault();
+    const emailInput = validation.emailinput.value;
+    var emailInputVal = document.getElementById("emailinput");
+    let alert22 = document.getElementById("woop22");
+    const patternForEmailInput = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (patternForEmailInput.test(emailInput)) {
+        emailInputVal.style.border = "1px solid green"
+        alert22.innerHTML = "";
+    }else {
+        emailInputVal.style.border = "1px solid red";
+        alert22.style.color = "red";
+        alert22.innerHTML = "Input is empty or insert '@'";
+    }
+})
